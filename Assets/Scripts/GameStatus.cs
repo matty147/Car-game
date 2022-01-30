037f6d57f6e0c184eb3e4725e55b0ff196df8a56
matty147 <matty.hana@gmail.com>
2022-01-30 19:08:28 +0100

    checkpoints

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+    private static int[] PlayerStatus = new int[] { 0, 0, 0, 0 };
+
+    private static readonly int CHECKPOINT_COUNT = 3;
+
+    public static void registerCheckpoint(string playerTag, int newCheckpoint)
+    {
+        int playerIdx = getPlayerIdx(playerTag);
+        int currentCheckpoint = PlayerStatus[playerIdx];
+        if (isOk(currentCheckpoint, newCheckpoint))
+        {
+            PlayerStatus[playerIdx] = newCheckpoint;
+            Debug.Log($"Checkpoint OK: player={playerTag}, checkpoint={currentCheckpoint} > {newCheckpoint}");
+        }
+        else
+        {
+            Debug.Log($"Checkpoint ERROR: player={playerTag}, checkpoint={currentCheckpoint} > {newCheckpoint}");
+        }
+    }
+
+    private static int getPlayerIdx(string playerTag)
+    {
+        switch (playerTag)
+        {
+            case "Player1": return 0;
+            case "Player2": return 1;
+            case "Player3": return 2;
+            case "Player4": return 3;
+        }
+        return -1;
+    }
+
+    private static bool isOk(int currentCheckpoint, int newCheckpoint)
+    {
+        if (newCheckpoint == 1)
+        {
+            return currentCheckpoint == 0 || currentCheckpoint == CHECKPOINT_COUNT;
+        }
+        else
+        {
+            return newCheckpoint == currentCheckpoint + 1;
+        }
+    }
+
+}
--- Assets/Scripts/Triggers/StartTriger1.cs
+++ Assets/Scripts/Triggers/StartTriger1.cs
@@ -4,38 +4,8 @@ using UnityEngine;
 
 public class StartTriger1 : MonoBehaviour
 {
-    private static float startp1, startp2, startp3, startp4;
-
-    void Start()
-    {
-        startp1 = -1f;
-        startp2 = -1f;
-        startp3 = -1f;
-        startp4 = -1f;
-    }
-
-
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player1")
-        {
-            startp1 = 1;
-            Debug.Log("c1P1");
-        }
-        if (other.tag == "Player2")
-        {
-            startp2 = 1;
-            Debug.Log("c1P2");
-        }
-        if (other.tag == "Player3")
-        {
-            startp3 = 1;
-            Debug.Log("c1P3");
-        }
-        if (other.tag == "Player4")
-        {
-            startp4 = 1;
-            Debug.Log("c1P4");
-        }
+        GameStatus.registerCheckpoint(other.tag, 1);
     }
 }
