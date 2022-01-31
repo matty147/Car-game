bcf456cfda3bf8cc7b93a818fc7086d474de125b
matty147 <matty.hana@gmail.com>
2022-01-31 19:14:07 +0100

    new map

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+   static public float time = 0.0f;
+    public Text timer;
+    public float counter;
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time < 0)
+        {
+            
+        }
+        //Debug.Log(time);
+        timer.text = time.ToString("0.0000");
+    }
+}
