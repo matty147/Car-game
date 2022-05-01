965221a1c36df0dd1969ea920b9b928eefa71e18
matty147 <matty.hana@gmail.com>
2022-05-01 12:37:39 +0200

    lapcounter

--- Assets/Scripts/LapsCounter.cs
+++ Assets/Scripts/LapsCounter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LapsCounter : MonoBehaviour
 {
@@ -15,6 +16,8 @@ public class LapsCounter : MonoBehaviour
 
     [SerializeField]
     GameObject Counter4;
+
+    public Text LapCounter1, LapCounter2, LapCounter3, LapCounter4;
 
     // Start is called before the first frame update
     void Start()
@@ -47,6 +50,32 @@ public class LapsCounter : MonoBehaviour
             Counter3.SetActive(true);//left down
             Counter4.SetActive(true); //right down
         }
-        Debug.Log($"{Checkpoints.PlayerLap}/4");
+        Debug.Log($"{Checkpoints.PlayerLap[0]}:{Checkpoints.PlayerLap[1]}:{Checkpoints.PlayerLap[2]}:{Checkpoints.PlayerLap[3]}");
+    }
+    private void FixedUpdate()
+    {
+       if (MainMenu.NumberOfPlayers == 1)
+        {
+            LapCounter4.text = Checkpoints.PlayerLap[0].ToString("0");
+        }
+        if (MainMenu.NumberOfPlayers == 2)
+        {
+            LapCounter1.text = Checkpoints.PlayerLap[0].ToString("0");
+            LapCounter4.text = Checkpoints.PlayerLap[1].ToString("0");
+        }
+        if (MainMenu.NumberOfPlayers == 3)
+        {
+            LapCounter2.text = Checkpoints.PlayerLap[0].ToString("0");
+            LapCounter1.text = Checkpoints.PlayerLap[1].ToString("0");
+            LapCounter3.text = Checkpoints.PlayerLap[2].ToString("0");
+        }
+        if (MainMenu.NumberOfPlayers == 4)
+        {
+            LapCounter2.text = Checkpoints.PlayerLap[0].ToString("0");
+            LapCounter1.text = Checkpoints.PlayerLap[1].ToString("0");
+            LapCounter3.text = Checkpoints.PlayerLap[2].ToString("0");
+            LapCounter4.text = Checkpoints.PlayerLap[3].ToString("0");
+        }
+
     }
 }
